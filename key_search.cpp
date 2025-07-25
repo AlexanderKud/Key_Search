764a29db1e05867c876da4d172d1a54d93560723
AlexCurl <144244712+AlexanderKud@users.noreply.github.com>
2025-07-26 00:17:26 +0300

    Update key_search.cpp

--- key_search.cpp
+++ key_search.cpp
@@ -151,8 +151,202 @@ auto main() -> int {
             addPointsNeg[i] = batch_Add;
             addPointsNeg[i].y.ModNeg();
         }
+        // center_key_search_save
+        auto center_key_search_save = [&](Point posStartP, Point negStartP, Int center_Num, Int stride_Sum) {
+
+            Int stride_sum; stride_sum.Set(&stride_Sum);
+            Int center_num; center_num.Set(&center_Num);
+            Int Int_steps, Int_temp, privkey;
+            int index, count;
+            uint64_t steps;
+            vector<uint64_t> privkey_num;
+            
+            IntGroup modGroupPos(POINTS_BATCH_SIZE); // group of deltaX (x1 - x2) set for batch inversion
+            IntGroup modGroupNeg(POINTS_BATCH_SIZE); // group of deltaX (x1 - x2) set for batch inversion
+            Int deltaXPos[POINTS_BATCH_SIZE]; // here we store (x1 - x2) batch that will be inverted for later multiplication
+            Int deltaXNeg[POINTS_BATCH_SIZE]; // here we store (x1 - x2) batch that will be inverted for later multiplication
+            modGroupPos.Set(deltaXPos); // assign array deltaX to modGroup for batch inversion (JLP way set it once)
+            modGroupNeg.Set(deltaXNeg); // assign array deltaX to modGroup for batch inversion (JLP way set it once)
+            Int pointBatchXPos[POINTS_BATCH_SIZE]; // X coordinates of the batch
+            Int pointBatchYPos[POINTS_BATCH_SIZE]; // Y coordinates of the batch
+            Int pointBatchXNeg[POINTS_BATCH_SIZE]; // X coordinates of the batch
+            Int pointBatchYNeg[POINTS_BATCH_SIZE]; // Y coordinates of the batch
+            Int deltaYPos, deltaYNeg; // values to store the results of points addition formula
+            Int slopePos[POINTS_BATCH_SIZE];
+            Int slopeNeg[POINTS_BATCH_SIZE];
+            
+            Point startPointPos = posStartP; // start point positive
+            Point startPointNeg = negStartP; // start point negative
+            Point BloomP;
+        
+            Int batch_stride, batch_index;
+            batch_stride.Mult(&stride, uint64_t(POINTS_BATCH_SIZE));
+
+            while (true) {
+
+                for (int i = 0; i < POINTS_BATCH_SIZE; i++) { // we compute (x1 - x2) for each entry of the entire batch
+                    deltaXPos[i].ModSub(&startPointPos.x, &addPointsPos[i].x);
+                    deltaXNeg[i].ModSub(&startPointNeg.x, &addPointsNeg[i].x);
+                }
+    
+                modGroupPos.ModInv();
+                modGroupNeg.ModInv();
+                
+                int i;
+                for (i = 0; i < POINTS_BATCH_SIZE - 1; i++) { // follow points addition formula logic
+                    
+                    deltaYPos.ModSub(&startPointPos.y, &addPointsPos[i].y);
+                    slopePos[i].ModMulK1(&deltaYPos, &deltaXPos[i]); // deltaX already inverted for each entry of the batch
+                    
+                    deltaYNeg.ModSub(&startPointNeg.y, &addPointsNeg[i].y);
+                    slopeNeg[i].ModMulK1(&deltaYNeg, &deltaXNeg[i]); // deltaX already inverted for each entry of the batch
+
+                    pointBatchXPos[i].ModSquareK1(&slopePos[i]); // computing just x coordinate for the (batch_size - 1)
+                    pointBatchXPos[i].ModSub(&pointBatchXPos[i], &startPointPos.x);
+                    pointBatchXPos[i].ModSub(&pointBatchXPos[i], &addPointsPos[i].x);
+                    
+                    pointBatchXNeg[i].ModSquareK1(&slopeNeg[i]); // computing just x coordinate for the (batch_size - 1)
+                    pointBatchXNeg[i].ModSub(&pointBatchXNeg[i], &startPointNeg.x);
+                    pointBatchXNeg[i].ModSub(&pointBatchXNeg[i], &addPointsNeg[i].x);
+                    
+                }
+                
+                deltaYPos.ModSub(&startPointPos.y, &addPointsPos[i].y);
+                slopePos[i].ModMulK1(&deltaYPos, &deltaXPos[i]);
+                
+                deltaYNeg.ModSub(&startPointNeg.y, &addPointsNeg[i].y);
+                slopeNeg[i].ModMulK1(&deltaYNeg, &deltaXNeg[i]);
+
+                pointBatchXPos[i].ModSquareK1(&slopePos[i]);
+                pointBatchXPos[i].ModSub(&pointBatchXPos[i], &startPointPos.x);
+                pointBatchXPos[i].ModSub(&pointBatchXPos[i], &addPointsPos[i].x);
+                
+                pointBatchXNeg[i].ModSquareK1(&slopeNeg[i]);
+                pointBatchXNeg[i].ModSub(&pointBatchXNeg[i], &startPointNeg.x);
+                pointBatchXNeg[i].ModSub(&pointBatchXNeg[i], &addPointsNeg[i].x);
+                    
+                pointBatchYPos[i].ModSub(&startPointPos.x, &pointBatchXPos[i]);
+                pointBatchYPos[i].ModMulK1(&slopePos[i], &pointBatchYPos[i]);
+                pointBatchYPos[i].ModSub(&pointBatchYPos[i], &startPointPos.y);
+                
+                pointBatchYNeg[i].ModSub(&startPointNeg.x, &pointBatchXNeg[i]);
+                pointBatchYNeg[i].ModMulK1(&slopeNeg[i], &pointBatchYNeg[i]);
+                pointBatchYNeg[i].ModSub(&pointBatchYNeg[i], &startPointNeg.y);
+
+                for (int i = 0; i < POINTS_BATCH_SIZE; i++) {
+                    // check positive
+                    if (bf.may_contain(pointBatchXPos[i].bits64[3])) {
+                        
+                        print_time(); cout << "BloomFilter Hit (C+) ";
+                        
+                        BloomP.x.Set(&pointBatchXPos[i]);
+                        BloomP.y.ModSub(&startPointPos.x, &pointBatchXPos[i]);
+                        BloomP.y.ModMulK1(&slopePos[i], &BloomP.y);
+                        BloomP.y.ModSub(&BloomP.y, &startPointPos.y);
+                        
+                        privkey_num.clear();
+                        index = 0;
+                        for (auto& p : pow10_points) {
+                            count = 0;
+                            while (bf.may_contain(BloomP.x.bits64[3])) {
+                                BloomP = secp256k1->SubtractPoints(BloomP, p);
+                                count += 1;
+                            }
+                            privkey_num.push_back(pow10_nums[index] * (count - 1));
+                            BloomP = secp256k1->AddPoints(BloomP, p);
+                            index += 1;
+                        }
+                        
+                        steps = 0;
+                        for (auto& n : privkey_num) { steps += n; }
+                        Int_steps.SetInt64(steps);
+                        batch_index.Mult(&stride, uint64_t(i + 1));
+                        Int_temp.Add(&stride_sum, &batch_index);
+                        center_num.Add(&Int_temp); 
+                        privkey.Sub(&center_num, &Int_steps);
+                        calc_point = secp256k1->ScalarMultiplication(&privkey);
+                        
+                        if (secp256k1->GetPublicKeyHex(calc_point) == search_pub) {
+                            cout << "-> Success" << endl;
+                            print_time(); cout << "Private key: " << privkey.GetBase10() << endl;
+                            ofstream outFile;
+                            outFile.open("found.txt", ios::app);
+                            outFile << privkey.GetBase10() << '\n';
+                            outFile.close();
+                            print_elapsed_time(chrono_start);
+                            exit(0);
+                        }
+                        
+                        cout << "-> False Positive" << endl;
+                    }
+                    // check negative
+                    if (bf.may_contain(pointBatchXNeg[i].bits64[3])) {
+                        
+                        print_time(); cout << "BloomFilter Hit (C-) ";
+                        
+                        BloomP.x.Set(&pointBatchXNeg[i]);
+                        BloomP.y.ModSub(&startPointNeg.x, &pointBatchXNeg[i]);
+                        BloomP.y.ModMulK1(&slopeNeg[i], &BloomP.y);
+                        BloomP.y.ModSub(&BloomP.y, &startPointNeg.y);
+                        
+                        privkey_num.clear();
+                        index = 0;
+                        for (auto& p : pow10_points) {
+                            count = 0;
+                            while (bf.may_contain(BloomP.x.bits64[3])) {
+                                BloomP = secp256k1->SubtractPoints(BloomP, p);
+                                count += 1;
+                            }
+                            privkey_num.push_back(pow10_nums[index] * (count - 1));
+                            BloomP = secp256k1->AddPoints(BloomP, p);
+                            index += 1;
+                        }
+                        
+                        steps = 0;
+                        for (auto& n : privkey_num) { steps += n; }
+                        Int_steps.SetInt64(steps);
+                        batch_index.Mult(&stride, uint64_t(i + 1));
+                        Int_temp.Add(&stride_sum, &batch_index);
+                        center_num.Sub(&Int_temp); 
+                        privkey.Sub(&center_num, &Int_steps);
+                        calc_point = secp256k1->ScalarMultiplication(&privkey);
+                        
+                        if (secp256k1->GetPublicKeyHex(calc_point) == search_pub) {
+                            cout << "-> Success" << endl;
+                            print_time(); cout << "Private key: " << privkey.GetBase10() << endl;
+                            ofstream outFile;
+                            outFile.open("found.txt", ios::app);
+                            outFile << privkey.GetBase10() << '\n';
+                            outFile.close();
+                            print_elapsed_time(chrono_start);
+                            exit(0);
+                        }
+                        
+                        cout << "-> False Positive" << endl;
+                    }
+                }
+                
+                startPointPos.x.Set(&pointBatchXPos[POINTS_BATCH_SIZE - 1]); // last batch entry as the new startPoint for the next batch iteration
+                startPointPos.y.Set(&pointBatchYPos[POINTS_BATCH_SIZE - 1]);
+                
+                startPointNeg.x.Set(&pointBatchXNeg[POINTS_BATCH_SIZE - 1]); // last batch entry as the new startPoint for the next batch iteration
+                startPointNeg.y.Set(&pointBatchYNeg[POINTS_BATCH_SIZE - 1]);
+                
+                stride_sum.Add(&batch_stride);
+                save_counter += 1;
+                
+                if (save_counter % 100000 == 0) {
+                    ofstream outFile;
+                    outFile.open("stride_sum.txt");
+                    outFile << stride_sum.GetBase10() << '\n';
+                    outFile.close();
+                    save_counter = 0;
+                    print_time(); cout << "Progress written to stride_sum.txt" << endl;
+                }
+            } // while (true) loop end curly brace
+        }; // center_key_search_save
         // center_key_search
-        auto center_key_search = [&](Point posStartP, Point negStartP, int threadIdx, Int center_Num, Int stride_Sum) {
+        auto center_key_search = [&](Point posStartP, Point negStartP, Int center_Num, Int stride_Sum) {
 
             Int stride_sum; stride_sum.Set(&stride_Sum);
             Int center_num; center_num.Set(&center_Num);
@@ -334,21 +528,10 @@ auto main() -> int {
                 
                 stride_sum.Add(&batch_stride);
                     
-                if (threadIdx == 0) {
-                    save_counter += 1;
-                    if (save_counter % 60000 == 0) {
-                        ofstream outFile;
-                        outFile.open("stride_sum.txt");
-                        outFile << stride_sum.GetBase10() << '\n';
-                        outFile.close();
-                        save_counter = 0;
-                        print_time(); cout << "Progress written to stride_sum.txt" << endl;
-                    }
-                }
             } // while (true) loop end curly brace
         }; // center_key_search
         // sideway_key_search
-        auto sideway_key_search = [&](Point posStartP, Point negStartP, int threadIdx, Int range_NumPos, Int range_NumNeg, Int stride_Sum) {
+        auto sideway_key_search = [&](Point posStartP, Point negStartP, Int range_NumPos, Int range_NumNeg, Int stride_Sum) {
 
             Int stride_sum; stride_sum.Set(&stride_Sum);
             Int range_num_pos; range_num_pos.Set(&range_NumPos);
@@ -536,9 +719,14 @@ auto main() -> int {
         
         std::thread keySearch_CThreads[cpuCores];
         std::thread keySearch_SThreads[cpuCores];
+        
+        keySearch_CThreads[0] = std::thread(center_key_search_save, pos_Points[0], neg_Points[0], center_Nums[0], stride_sum);
+        for (int i = 1; i < cpuCores; i++) {
+            keySearch_CThreads[i] = std::thread(center_key_search, pos_Points[i], neg_Points[i], center_Nums[i], stride_sum);
+        }
+        
         for (int i = 0; i < cpuCores; i++) {
-            keySearch_CThreads[i] = std::thread(center_key_search, pos_Points[i], neg_Points[i], i, center_Nums[i], stride_sum);
-            keySearch_SThreads[i] = std::thread(sideway_key_search, pos_PointsSw[i], neg_PointsSw[i], i, range_Nums[i], range_Nums[i + 1], stride_sum);
+            keySearch_SThreads[i] = std::thread(sideway_key_search, pos_PointsSw[i], neg_PointsSw[i], range_Nums[i], range_Nums[i + 1], stride_sum);
         }
 
         for (int i = 0; i < cpuCores; i++) {
